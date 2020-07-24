fa2cd8049be7ae4591b3d6a91ce091233b47ad8e
Petr Walczysko <p.walczysko@dundee.ac.uk>
2020-07-24 12:14:48 +0100

    Exchange the image to attach the files to in script

--- experimentA/upload_attachments.py
+++ experimentA/upload_attachments.py
@@ -69,7 +69,7 @@ def process_line(conn, project, line, link_image, count):
             for image in dataset.linkedImageList():
                 image_name_dot = image.getName().getValue().split('.')
                 length = len(image_name_dot)
-                image_ending_right = (image_name_dot[length-2] == 'WB')
+                image_ending_right = (image_name_dot[length-2] == 'astroP65')
                 if (image_ending_right):
                     link(conn, image, line, link_image)
                     print("Linked attachment %s to image %s" %
